84fbdc99e3da45c5bfd2e7f2ddd426532ef39822
Pete Coleman <pcoleman@marex.com>
2025-10-28 07:14:25 +0000

    feat(bindings): remove the window navigation bindings, C-w isn't inconvenient enough

--- lua/general/bindings.lua
+++ lua/general/bindings.lua
@@ -4,12 +4,6 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", {})
 -- Terminal
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Window Navigation
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
 -- Execute Lua
 vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = "Execute line (Lua)" })
 vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Execute selection (Lua)" })
