9df43939381105021748802b13065ca58f416cf0
Pete Coleman <pcoleman@marex.com>
2025-12-02 11:45:06 +0000

    feat(dashboard): add neogit shortcut to dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,4 +1,4 @@
-return {
+﻿return {
   {
     "folke/snacks.nvim",
     lazy = false,
@@ -23,6 +23,7 @@ return {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = "", key = "g", desc = "Neogit", action = ":Neogit" },
             { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
             { icon = " ", key = "M", desc = "Mason", action = ":Mason" },
             { icon = "󰒍", key = "C", desc = "Checkhealth", action = ":checkhealth" },
